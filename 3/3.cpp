5f4c9b818866b2bcbb76af9ea0a576eace0136b9
PospelovAndrey <78700150+PospelovAndrey@users.noreply.github.com>
2024-04-04 13:38:11 +0500

    Лабораторные 4,5,6. Добаботка 3 для замера времени выполнения.

--- 3/3.cpp
+++ 3/3.cpp
@@ -1,117 +1,111 @@
 ﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
 #include <omp.h>
 #include <windows.h>
+
 using namespace std;
 
-#define MAX_SIZE 100
+#define SIZE 3000
 
-void printMatrix(double matrix[MAX_SIZE][MAX_SIZE + 1], int size) 
-{
-    for (int i = 0; i < size; i++) 
-    {
-        for (int j = 0; j <= size; j++) 
-        {
-            cout << matrix[i][j] << "\t";
+void printMatrix(const vector<vector<double>>& matrix) {
+    for (const auto& row : matrix) {
+        for (double elem : row) {
+            cout << elem << "\t";
         }
         cout << endl;
     }
 }
 
-void ForwardRunning(double A[MAX_SIZE][MAX_SIZE + 1], double x[MAX_SIZE], int size)
-{
-    int row, col;
-    for (col = 0; col < size - 1; col++)
-    {
+void ForwardRunning(vector<vector<double>>&A) {
+    int row;
+    for (int col = 0; col < SIZE - 1; col++) {
 #pragma omp parallel for shared(A) private(row)
-        for (row = col + 1; row < size; row++)
-        {
-            if (A[col][col] == 0) {
-
-                int swap_row = -1;
-                for (int k = col + 1; k < size; k++)
-                {
-                    if (A[k][col] != 0)
-                    {
-                        swap_row = k;
-                        break;
-                    }
-                }
-                if (swap_row == -1)
-                {
-                    cerr << "\nСистема не имеет решений или их кол-во бесконечно" << endl;
-                    exit(1);
-                }
-
-                for (int k = col; k <= size; k++)
-                {
-                    double temp = A[col][k];
-                    A[col][k] = A[swap_row][k];
-                    A[swap_row][k] = temp;
-                }
-            }
-
+        for (row = col + 1; row < SIZE; row++) {
             double ratio = A[row][col] / A[col][col];
-            for (int i = col; i <= size; i++) {
+            for (int i = col; i <= SIZE; i++) {
                 A[row][i] -= ratio * A[col][i];
             }
         }
     }
 }
 
-void ReverseRunning(double A[MAX_SIZE][MAX_SIZE + 1], double x[MAX_SIZE], int size) 
-{
-    int row, col;
-    for (row = size - 1; row >= 0; row--) 
-    {
-        x[row] = A[row][size];
-        for (col = row + 1; col < size; col++) 
-        {
+void ReverseRunning(const vector<vector<double>>& A, vector<double>& x) {
+    for (int row = SIZE - 1; row >= 0; row--) {
+        x[row] = A[row][SIZE];
+        for (int col = row + 1; col < SIZE; col++) {
             x[row] -= A[row][col] * x[col];
         }
         x[row] /= A[row][row];
     }
 }
 
-int main() 
-{
+void generateRandomSystem(vector<vector<double>>& A) {
+    for (int i = 0; i < SIZE; ++i) {
+        vector<double> row(SIZE + 1);
+        int sum = 0;
+        for (int j = 0; j <= SIZE; ++j) {
+            if (j != SIZE) {
+                int num;
+                do {
+                    num = rand() % 10;
+                } while (num == 3 || num == 7 || num == 6 || num == 9);
+                row[j] = num;
+                sum += num;
+            }
+            else {
+                row[j] = sum;
+            }
+        }
+        A.push_back(row);
+    }
+}
 
+void writeVectorToFile(const string& filename, const vector<vector<double>>& data) {
+    ofstream outFile(filename);
+    for (const auto& row : data) {
+        for (double elem : row) {
+            outFile << elem << " ";
+        }
+        outFile << endl;
+    }
+    outFile.close();
+}
+
+void readVectorFromFile(const string& filename, vector<vector<double>>& data) {
+    ifstream inFile(filename);
+    double value;
+    vector<double> row;
+    while (inFile >> value) {
+        row.push_back(value);
+        if (row.size() == SIZE + 1) {
+            data.push_back(row);
+            row.clear();
+        }
+    }
+    inFile.close();
+}
+
+int main() {
+    const string filename = "array.txt";
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
+    vector<double> x(SIZE);
 
-    int size;
-    double A[MAX_SIZE][MAX_SIZE + 1]; 
-    double x[MAX_SIZE];                
+    /*vector<vector<double>> A;
+    generateRandomSystem(A);
+    writeVectorToFile(filename, A);*/
+    vector<vector<double>> readArray;
+    readVectorFromFile(filename, readArray);
 
-    cout << "Введите количество уравнений и неизвестных: ";
-    cin >> size;
-
-    cout << "Введите коэффициенты уравнений:\n";
-    for (int i = 0; i < size; i++) 
-    {
-        cout << "\nУравнение " << i + 1 << ":\n";
-        for (int j = 0; j < size; j++) 
-        {
-            cout << "Коэффициент для переменной " << j + 1 << ": ";
-            cin >> A[i][j];
-        }
-        cout << "Введите свободный член: ";
-        cin >> A[i][size];
-    }
-
-    cout << "\nМатрица A до преобразований:\n";
-    printMatrix(A, size);
-
-    ForwardRunning(A, x, size);
-    ReverseRunning(A, x, size);
-
-    cout << "\nМатрица A после преобразований:\n";
-    printMatrix(A, size);
-
-    cout << "\nРешение:\n";
-    for (int i = 0; i < size; i++)
-    {
-        cout << "x"<< i+1 << " = " << x[i] << "\n";
-    }
+    clock_t start = clock();
+    ForwardRunning(readArray);
+    ReverseRunning(readArray, x);
+    clock_t end = clock();
+    double duration = static_cast<double>(end - start) / CLOCKS_PER_SEC;
+    cout << "Время выполнения: " << duration * 1000 << " миллисекунд." << endl;
 
     return 0;
 }
---
+++ 6/6.cpp
@@ -0,0 +1,114 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+#include <cstdlib>
+#include <ctime>
+#include <windows.h>
+#include <tbb/parallel_for.h>
+#include <tbb/blocked_range.h>
+
+
+using namespace std;
+using namespace tbb;
+
+#define SIZE 3000
+
+void printMatrix(const vector<vector<double>>& matrix) {
+    for (const auto& row : matrix) {
+        for (double elem : row) {
+            cout << elem << "\t";
+        }
+        cout << endl;
+    }
+}
+
+void ForwardRunning(vector<vector<double>>& A) {
+    for (int col = 0; col < SIZE - 1; col++) {
+        parallel_for(blocked_range<size_t>(col + 1, SIZE), [&](const blocked_range<size_t>& r) {
+        for (int row = r.begin(); row < r.end(); row++) {
+            double ratio = A[row][col] / A[col][col];
+            for (int i = col; i <= SIZE; i++) {
+                A[row][i] -= ratio * A[col][i];
+            }
+        }
+        });
+    }
+}
+
+void ReverseRunning(const vector<vector<double>>& A, vector<double>& x) {
+    for (int row = SIZE - 1; row >= 0; row--) {
+        x[row] = A[row][SIZE];
+        for (int col = row + 1; col < SIZE; col++) {
+            x[row] -= A[row][col] * x[col];
+        }
+        x[row] /= A[row][row];
+    }
+}
+
+void generateRandomSystem(vector<vector<double>>& A) {
+    for (int i = 0; i < SIZE; ++i) {
+        vector<double> row(SIZE + 1);
+        int sum = 0;
+        for (int j = 0; j <= SIZE; ++j) {
+            if (j != SIZE) {
+                int num;
+                do {
+                    num = rand() % 10;
+                } while (num == 3 || num == 7 || num == 6 || num == 9);
+                row[j] = num;
+                sum += num;
+            }
+            else {
+                row[j] = sum;
+            }
+        }
+        A.push_back(row);
+    }
+}
+
+void writeVectorToFile(const string& filename, const vector<vector<double>>& data) {
+    ofstream outFile(filename);
+    for (const auto& row : data) {
+        for (double elem : row) {
+            outFile << elem << " ";
+        }
+        outFile << endl;
+    }
+    outFile.close();
+}
+
+void readVectorFromFile(const string& filename, vector<vector<double>>& data) {
+    ifstream inFile(filename);
+    double value;
+    vector<double> row;
+    while (inFile >> value) {
+        row.push_back(value);
+        if (row.size() == SIZE + 1) {
+            data.push_back(row);
+            row.clear();
+        }
+    }
+    inFile.close();
+}
+
+int main() {
+    const string filename = "array.txt";
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    vector<double> x(SIZE);
+
+    /*vector<vector<double>> A;
+    //generateRandomSystem(A);
+    writeVectorToFile(filename, A);*/
+    vector<vector<double>> readArray;
+    readVectorFromFile(filename, readArray);
+
+    clock_t start = clock();
+    ForwardRunning(readArray);
+    ReverseRunning(readArray, x);
+    clock_t end = clock();
+    double duration = static_cast<double>(end - start) / CLOCKS_PER_SEC;
+    cout << "Время выполнения: " << duration * 1000 << " миллисекунд." << endl;
+
+    return 0;
+}
